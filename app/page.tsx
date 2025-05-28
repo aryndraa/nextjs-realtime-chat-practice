cb4a434e767f3b4f048c57b1b7340044d17c8f2a
aryndraa <aryacoder1102@gmail.com>
2025-05-28 12:47:21 +0800

    setup supabase

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
         <div className="h-20">
           <div className="p-5 border-b flex items-center justify-between">
             <div>
-              <h1 className="text-xl font-bold">Daily Chat</h1>
+              <h1 className="text-xl font-bold mb-2">Daily Chat</h1>
               <div className="flex items-center gap-1">
                 <div className="size-4 bg-green-500 rounded-full animate-pulse"></div>
                 <h1 className="text-sm text-gray-400">2 Onlines</h1>
