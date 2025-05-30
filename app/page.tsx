45ad7c0b63c773ef88ac79abfddb6e3573c3ceca
aryndraa <aryacoder1102@gmail.com>
2025-05-30 16:01:29 +0800

    init user state

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import ChatHeader from "@/components/ui/ChatHeader";
+import InitUser from "@/lib/store/InitUser";
 import { supabaseBrowser } from "@/utils/supabase/browser";
 import React from "react";
 
@@ -7,10 +8,13 @@ export default async function Page() {
   const { data } = await supabase.auth.getSession();
 
   return (
-    <div className="max-w-3xl mx-auto md:py-10 h-screen">
-      <div className="h-full border rounded-md">
-        <ChatHeader user={data.session?.user} />
+    <>
+      <div className="max-w-3xl mx-auto md:py-10 h-screen">
+        <div className="h-full border rounded-md">
+          <ChatHeader user={data.session?.user} />
+        </div>
       </div>
-    </div>
+      <InitUser user={data.session?.user} />
+    </>
   );
 }
